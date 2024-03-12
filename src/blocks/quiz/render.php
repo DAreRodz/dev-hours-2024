4c0ea2945fcb13f17b4c588612327f2bc110ce28
David Arenas <david.arenas@automattic.com>
2024-03-12 14:47:40 +0100

    step 3

--- src/blocks/quiz/render.php
+++ src/blocks/quiz/render.php
@@ -1,7 +1,12 @@
+<?php
+$unique_id = substr(uniqid(), -5);
+$context   = array( 'isOpen' => false );
+?>
+
 <div
 	<?php echo get_block_wrapper_attributes(); ?>
 	data-wp-interactive="dev-hours/quiz"
-	<?php echo wp_interactivity_data_wp_context( array( 'isOpen' => false ) ); ?>
+	<?php echo wp_interactivity_data_wp_context( $context ); ?>
 >
 	<div>
 		<strong>
@@ -10,12 +15,19 @@
 
 		<?php echo $attributes[ 'question' ]; ?>
 
-		<button data-wp-on--click="actions.toggle">
+		<button
+			data-wp-on--click="actions.toggle"
+			data-wp-bind--aria-expanded="context.isOpen"
+			aria-controls="quiz-<?php echo $unique_id; ?>"
+		>
 			<?php echo __( 'Open' ); ?>
 		</button>
 	</div>
 
-	<div data-wp-bind--hidden="!context.isOpen">
+	<div
+		data-wp-bind--hidden="!context.isOpen"
+		id="quiz-<?php echo $unique_id; ?>"
+	>
 		<?php if ( $attributes['typeOfQuiz'] == 'boolean' ): ?>
 			<button>
 				<?php echo __( 'Yes' ); ?>
